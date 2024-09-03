9bbcb66b9b5f4e4481d68106eda93ecd87ba262f
Widedbounou <wided.bounouara@gmail.com>
2024-09-03 17:26:27 +0400

    Update isAuthenticated.js

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,4 +1,4 @@
-// Import du modele User car utilisation pour savoir si le client est dans BDD pour login ou non
+
 const User = require("../models/User");
 
 const isAuthenticated = async (req, res, next) => {
@@ -6,14 +6,13 @@ const isAuthenticated = async (req, res, next) => {
     if (!req.headers.authorization) {
       return res.status(401).json({ message: "Unauthorized" });
     }
-    const token = req.headers.authorization.replace("Bearer ", ""); // la varia Token permet de supprimer le Bearer  affiché apres le token pour comparer ensuite
+    const token = req.headers.authorization.replace("Bearer ", "");
     const user = await User.findOne({ token: token });
 
     if (!user) {
       return res.status(401).json({ message: "Unauthorized" });
     }
-    // par convention user existe donc ici
-    //on stocke dans une nouvelle requete l'user qui s'est bien identifié pour opti code dans les autres routes
+  
     req.user = user;
     return next();
   } catch (error) {
